afb8027f3cfb8c4b4f7e7f8ed639360211c2b4d5
uoo38 <plus.wisteria38@gmail.com>
2018-07-10 12:18:25 +0900

    Implement Abstract Syntax Tree.

--- main.c
+++ main.c
@@ -2,6 +2,35 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+enum {
+  AST_INT,
+  AST_OP_ADD,
+  AST_OP_SUB,
+};
+
+typedef struct _Ast {
+  int type;
+  int ival;
+  struct _Ast *left;
+  struct _Ast *right;
+} Ast;
+
+Ast *make_ast_op(int type, Ast *left, Ast *right) {
+  Ast *p = malloc(sizeof(Ast));
+  p->type = type;
+  p->left = left;
+  p->right = right;
+  return p;
+}
+
+Ast *make_ast_int(int val) {
+  Ast *p = malloc(sizeof(Ast));
+  p->type = AST_INT;
+  p->ival = val;
+  p->left = p->right = NULL;
+  return p;
+}
+
 void skip(void) {
   char c;
   do {
@@ -10,76 +39,99 @@ void skip(void) {
   ungetc(c, stdin);
 }
 
-int read_number(void) {
+void error(char *s) {
+  fprintf(stderr, "Error: %s.", s);
+  exit(1);
+}
+
+Ast *read_number(void) {
   char c;
-  int ret = 0;
+  int num = 0;
 
   skip();
+
+  c = getc(stdin);
+  if (!isdigit(c))
+    error("number was expected");
+  else
+    ungetc(c, stdin);
 
   while ((c = getc(stdin)) != EOF) {
     if (!isdigit(c)) {
       ungetc(c, stdin);
       break;
     }
-    ret = ret * 10 + c - '0';
+    num = num * 10 + c - '0';
   }
 
-  return ret;
-}
-
-void error(char *s) {
-  fprintf(stderr, "Error: %s.", s);
-  exit(1);
+  return make_ast_int(num);
 }
 
 /*
-  expression ::= number | number [ expression_add | expression_sub ]
-  expression_add ::= '+' expression
-  expression_sub ::= '-' expression
+  <expr> = <number> <expr_tail>
+  <expr_tail> = ε | '+' <number> <expr_tail> | '-' <number> <expr_tail>
 */
-void expression(char operation) {
-  int val;
-  char c;
+Ast *expr_tail(Ast *left) {
+  skip();
+  char c = getc(stdin);
 
-  if (operation == 0) {
-    val = read_number();
-    printf("\tmovq $%d, %%rax\n", val);
-    skip(); c = getc(stdin);
-  } else {
-    printf("\tpopq %%rax\n");
-    c = operation;
-  }
-
-  switch (c) {
-  case '+':
-  case '-':
-    val = read_number();
-    char *op = c == '+' ? "add" : "sub";
-    printf("\t%s $%d, %%rax\n", op, val);
-    printf("\tpushq %%rax\n");
-    skip(); c = getc(stdin);
-    if (c == '+')
-      expression('+');
-    else if (c == '-')
-      expression('-');
-    else if (c == '\n' || c == EOF)
-      return;
-    else
-      error("unexpected character was inputted");
-    break;
-  case '\n':
-  case EOF:
-    printf("\tpushq %%rax\n");
-    return;
-  default:
+  if (c == '+' || c == '-') {
+    Ast *right = read_number();
+    int ast_op = c == '+' ? AST_OP_ADD : AST_OP_SUB;
+    Ast *p = make_ast_op(ast_op, left, right);
+    return expr_tail(p);
+  } else if (c == '\n' || c == EOF)
+    return left;
+  else
     error("unexpected character was inputted");
+
+  return NULL; // NOTE: To avoid warning. Actually, cannot reach here.
+}
+
+Ast *expr(void) {
+  Ast *val = read_number();
+  return expr_tail(val);
+}
+
+void debug_print(Ast *p) {
+  if (p == NULL)
+    return;
+  else if (p->type == AST_INT)
+    printf("%d", p->ival);
+  else if (p->type == AST_OP_ADD || p->type == AST_OP_SUB) {
+    printf("(%c ", p->type == AST_OP_ADD ? '+' : '-');
+    debug_print(p->left);
+    printf(" ");
+    debug_print(p->right);
+    printf(")");
+  }
+}
+
+void codegen(Ast *p) {
+  if (p == NULL)
+    return;
+
+  switch (p->type) {
+  case AST_INT:
+    printf("\tpushq $%d\n", p->ival);
+    break;
+  case AST_OP_ADD:
+  case AST_OP_SUB:
+    codegen(p->left);
+    codegen(p->right);
+    printf("\tpopq %%rbx\n");
+    printf("\tpopq %%rax\n");
+    printf("\t%s %%rbx, %%rax\n", p->type == AST_OP_ADD ? "add" : "sub");
+    printf("\tpushq %%rax\n");
   }
 }
 
 int main(void) {
+  Ast *root = expr();
+
   printf("\t.global main\n");
   printf("main:\n");
-  expression(0);
+  codegen(root);
   printf("\tpopq %%rax\n");
   printf("\tret\n");
 
