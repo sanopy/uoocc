b4c9ddaf48751321f6e7f06a629fa7ffcba148e0
Sanosuke Kato <plus.wisteria38@gmail.com>
2018-08-17 13:34:43 +0900

    Add knapsac problem solver as sample.

---
+++ sample/knapsac.c
@@ -0,0 +1,35 @@
+int N;
+int W;
+int v[128];
+int w[128];
+int memo[128][10000];
+
+int solve(int i, int weight) {
+  if (i == N && weight <= W)
+    return 0;
+  else if (i >= N || weight > W)
+    return  0 - 9999999;
+  else if (memo[i][weight] != 0)
+    return memo[i][weight];
+  else {
+    int a;
+    int b;
+    a = solve(i + 1, weight);
+    b = solve(i + 1, weight + w[i]) + v[i];
+    if (a < b)
+      return b;
+    else 
+      return a;
+  }
+}
+
+int main() {
+  int i;
+  scanf("%d %d", &N, &W);
+  for (i = 0; i < N; i++) {
+    scanf("%d %d", v + i, w + i);
+  }
+  printf("%d\n", solve(0, 0));
+
+  return 0;
+}
