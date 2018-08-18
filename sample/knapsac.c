08bfe28af6e3575a9be044da0d9225d8ba90a268
Sanosuke Kato <plus.wisteria38@gmail.com>
2018-08-18 10:06:39 +0900

    Fix to memoization.

--- sample/knapsac.c
+++ sample/knapsac.c
@@ -2,13 +2,13 @@ int N;
 int W;
 int v[128];
 int w[128];
-int memo[128][10000];
+int memo[128][10001];
 
 int solve(int i, int weight) {
   if (i == N && weight <= W)
     return 0;
   else if (i >= N || weight > W)
-    return  0 - 9999999;
+    return 0 - 99999;
   else if (memo[i][weight] != 0)
     return memo[i][weight];
   else {
@@ -17,18 +17,17 @@ int solve(int i, int weight) {
     a = solve(i + 1, weight);
     b = solve(i + 1, weight + w[i]) + v[i];
     if (a < b)
-      return b;
-    else 
-      return a;
+      return memo[i][weight] = b;
+    else
+      return memo[i][weight] = a;
   }
 }
 
 int main() {
   int i;
   scanf("%d %d", &N, &W);
-  for (i = 0; i < N; i++) {
+  for (i = 0; i < N; i++)
     scanf("%d %d", v + i, w + i);
-  }
   printf("%d\n", solve(0, 0));
 
   return 0;
